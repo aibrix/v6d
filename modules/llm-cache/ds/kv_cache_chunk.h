06be803b99e65f2a261bbd223807358be3d8298d
DwyaneShi <dwyane.shi@gmail.com>
2024-11-11 14:32:21 -0800

    Enhance comments for major components
    Signed-off-by: DwyaneShi <dwyane.shi@gmail.com>

--- modules/llm-cache/ds/kv_cache_chunk.h
+++ modules/llm-cache/ds/kv_cache_chunk.h
@@ -39,6 +39,24 @@ namespace vineyard {
 // forward declaration
 struct LLMKV;
 
+// A KVCacheChunk contains all the KV tensors of a fixed number of
+// tokens (i.e., `chunk_size`).
+//
+// In its object blob, we first store all the KV tensors, and then
+// store all the tokens (including prefix tokens and current tokens
+// cached in the chunk), which will be used to avoid hash conflicts.
+//
+// In its metadata, we store the namespace (i.e., `ns_`), which will
+// be used as the name prefix of each chunk. Clients can also use the
+// namespace to list all the chunks. Access time (i.e., 'access_time_`)
+// in its metadata is used for the TTL-based global GC. We also have
+// the md5sum of all tokens (including prefix tokens and current tokens)
+// in its metadata. When we reconstruct a chunk from the object blob
+// and metadata, we calculate the md5sum of all tokens in the blob and
+// compare it with the md5sum in the metadata. If they are the same,
+// we consider the chunk is valid. Otherwise, we consider the chunk is
+// corrupted. By far, we don't use the md5sum of the tensors to alleviate
+// the compute overhead.
 class KVCacheChunk : public vineyard::Registered<KVCacheChunk> {
  public:
   inline static constexpr char kFieldNameNS[] = "namespace";
@@ -52,12 +70,17 @@ class KVCacheChunk : public vineyard::Registered<KVCacheChunk> {
 
  private:
   std::shared_ptr<Buffer> buffer_;
+  // number of prefix tokens and current tokens in the chunk
   int total_tokens_;
   int tensor_nbytes_;
   int layer_;
   int chunk_size_;
+  // access time is used for TTL-based global GC
   uint64_t access_time_;
+  // md5sum of all tokens (including prefix tokens and current tokens)
   std::string md5_;
+  // namespace. chunks within the same namespace will be shared
+  // among different clients
   std::string ns_;
 
  public:
@@ -79,6 +102,29 @@ class KVCacheChunk : public vineyard::Registered<KVCacheChunk> {
   friend class KVCacheChunkBuilder;
 };
 
+// A KVCacheChunkBuilder is used to build a KVCacheChunk.
+//
+// We have two kinds of builders:
+// 1. The builder to build a new chunk.
+// 2. The builder to rebuild a chunk from the object blob and metadata.
+//
+// For the first kind of builder, `Make` creates an empty chunk and an
+// `Update` filles the chunk with KV tensors. After `Update`, the chunk
+// is marked as ready and waiting readers will be notified. This kind
+// of builder can be sealed to a KVCacheChunk.
+//
+// For the second kind of builder, `Make` only assignes the chunk id and
+// the first `Query` will trigger a construction of the chunk, i.e.,
+// constructing the corresponding chunk with fetched metadata and blob.
+// After construction, the chunk is marked as ready and other waiting
+// readers will be notified. This kind of builder will never be sealed
+// since the chunk already exists in the object store.
+//
+// We also track the access time of the chunk in the builder. Global
+// access time is the latest access time of the global object we know.
+// Access time is the local access time that is updated by each access.
+// The local access time will finally be updated to the global access
+// time based on the policy used in AIBrixBlobStorage.
 class KVCacheChunkBuilder {
  private:
   RPCClient& rpc_client_;
@@ -86,16 +132,23 @@ class KVCacheChunkBuilder {
   std::shared_ptr<RemoteBlobWriter> remote_buffer_writer_ = nullptr;
   ObjectID chunk_id_;
   std::shared_ptr<Buffer> buffer_ = nullptr;
+
   int total_tokens_;
   int tensor_nbytes_;
   int layer_;
   int chunk_size_;
   std::string ns_;
+
+  // `time_mu_` protects the access times of the chunk.
   std::shared_mutex time_mu_;
   uint64_t g_access_time_ = 0;
   uint64_t access_time_ = 0;
+
+  // `mutex_` and `cv_` are used to block readers until the chunk
+  // is ready to be read.
   std::mutex mutex_;
   std::condition_variable cv_;
+
   std::atomic<bool> is_ready_ = false;
   std::string md5_;
 
@@ -140,6 +193,7 @@ class KVCacheChunkBuilder {
     return access_time_;
   }
 
+  // Whether the chunk is ready to be read.
   bool IsReady() { return is_ready_; }
 
   std::shared_ptr<Object> Seal();
@@ -150,6 +204,7 @@ class KVCacheChunkBuilder {
 
   void PrintKVCacheChunk();
 
+  // Whether the chunk is the same as the chunk with the given metadata.
   Status IsSame(const ObjectMeta& meta);
 
   KVCacheChunkBuilder(RPCClient& rpc_client, int tensor_nbytes, int layer,
